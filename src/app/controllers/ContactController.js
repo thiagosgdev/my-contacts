ec913b77d570d7a485fbcdae5fb94a76f8e12e50
Thiago Santos Gonçalves <thiagosgdev@gmail.com>
2021-12-14 11:32:23 -0300

    feat: Created contacts routes and controller

---
+++ src/app/controllers/ContactController.js
@@ -0,0 +1,23 @@
+class ContactController {
+    index(request, response) {
+        response.send("Sent from Contact Controller");
+    }
+
+    show() {
+
+    }
+
+    store() {
+
+    }
+
+    update() {
+
+    }
+
+    delete() {
+
+    }
+}
+
+module.exports = new ContactController;
