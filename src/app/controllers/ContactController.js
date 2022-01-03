c90dac2160b307f99fed921e0e9f9fcd5c7af806
Thiago Santos Gonçalves <thiagosgdev@gmail.com>
2022-01-03 16:24:25 -0300

    feat: route update creation and configuration

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -23,10 +23,13 @@ class ContactController {
     async store(request, response) {
         const { name, email, phone, category_id } = request.body;
 
+        if(!name){
+            return response.status(400).json({error: "Field name is required!"});
+        }
         const contactExists = await ContactsRepository.findByEmail(email);
 
         if (contactExists) {
-            return response.status(400).json({error: "This e-mail is already been taken!"});
+            return response.status(400).json({error: "This e-mail is already in use!"});
         }
 
         const contact = await ContactsRepository.create({
@@ -36,11 +39,33 @@ class ContactController {
         response.json(contact)
     }
 
-    update() {
+    async update(request, response) {
+        const { id } = request.params;
+        const { name, email, phone, category_id } = request.body;
+
+        const contactExists = await ContactsRepository.findById(id);
+        if(!contactExists){
+            return response.status(404).json({ error: "User not found!"});
+        }
+
+        if(!name){
+            return response.status(400).json({error: "Field name is required!"});
+        }
+        const contactByEmail = await ContactsRepository.findByEmail(email);
+
+        if (contactExists && contactByEmail.id !== id) {
+            return response.status(400).json({error: "This e-mail is already in use!"});
+        }
+
+        const contact = await ContactsRepository.update(id, {
+            name, email, phone, category_id
+        });
+
+        response.json(contact);
 
     }
 
-    delete() {
+    async delete() {
         const { id } = request.params;
 
         const contact = await ContactsRepository.findById(id);
